0ce4423a45eaf044f63c7cdbe6e0eb73428189a2
Reimar <mail@reim.ar>
2025-02-17 10:55:29 +0100

    Use decimal numbers for noise

--- 0.Dataanalyse/NumPy.py
+++ 0.Dataanalyse/NumPy.py
@@ -5,7 +5,7 @@ print(feature)
 label = np.vectorize(lambda n: 3 * n + 4)(feature)
 print(label)
 
-noise = np.random.randint(low=-2, high=3, size=(len(feature)))
+noise = np.random.random(len(feature)) * 4 - 2
 print(noise)
 label = np.add(label, noise)
 print(label)
